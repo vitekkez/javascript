5e442366b393fe2ebf19e7731a963edc8859a213
Viktor Kashko <vitekkez@gmail.com>
2022-09-15 20:47:50 +0300

    finish test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,48 @@
+let myAge = 43,
+  myWife = 36;
+
+function testBlock() {
+  let myAge = 18;
+  console.log(myAge);
+}
+testBlock();
+
+myAge = 27;
+
+myNewAge = myWife;
+
+let myName = "Victor";
+
 console.log("hello word!!!");
+console.log(myName);
+console.log(myAge);
+console.log(myNewAge);
+
+const userProfile = {
+  name: "Віктор",
+  age: 43,
+  message: "how are you!",
+};
+console.log(userProfile);
+
+userProfile.age = 22;
+userProfile.neme = "Alina";
+
+console.log | userProfile;
+
+// Домашнє завдання
+
+let user = "Вася";
+let age = "36";
+
+console.log(user);
+console.log(age);
+
+const BLOCK_SIZE = 14 + 50;
+
+console.log(BLOCK_SIZE);
+
+if (true) {
+  var size = 15;
+}
+console.log(size);
